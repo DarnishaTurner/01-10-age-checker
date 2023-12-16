57590e6aac3352d4d9d7135936ffb8dc6ea55c68
DarnishaTurner <darnishaturner@pursuit.org>
2023-12-16 10:04:12 -0500

    Darnisha finished work

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const ages = [5, 15, 25, 70];
 ages.forEach(age => {
   let category = '';
 
-  // Write your if-else statements here using specified age ranges
-
+if (age < 13) {
+  category = `child`;
+} else if (age >= 13 && age <= 19) {
+  category = `teen`;
+} else if (age >= 20 && age <= 64){
+  category = `adult`;
+} else {
+  category = `senior`;
+}
   console.log(`Age ${age}: ${category}`);
 });
